f29718a293111fb541269144e698803b8aaecb3e
Ved Prakash Vishwakarma <vedprakash22@iitk.ac.in>
2025-12-27 21:17:05 +0530

    Minor fix in axiosInstance in frontend-backend api connection

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
   timeout: 30000, // Increase timeout for file uploads
 });
 
@@ -12,13 +12,13 @@ axiosInstance.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    
+
     // Don't set Content-Type for FormData (multipart/form-data)
     if (config.data instanceof FormData) {
       // Let the browser set the Content-Type with boundary
       delete config.headers['Content-Type'];
     }
-    
+
     return config;
   },
   (error) => {
